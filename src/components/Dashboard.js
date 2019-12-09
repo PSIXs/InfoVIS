162c26d6cee0037c041b3f7c24d09c13c05d6c50
erkin97 <erkin97@unist.ac.kr>
2019-12-09 20:57:18 +0900

    Initialize

---
+++ src/components/Dashboard.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Container from '@material-ui/core/Container';
+import Box from '@material-ui/core/Box';
+
+export default function App() {
+  return (
+    <Container maxWidth="sm">
+      <Box my={4}>
+        Dashboard starts here
+      </Box>
+    </Container>
+  );
+}
